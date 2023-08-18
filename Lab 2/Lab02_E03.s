673dd83bb0486a21f62003aa7fdb9ae2fca982ec
Chathura De Silva <97898278+chathura7357@users.noreply.github.com>
2023-08-19 00:29:38 +0530

    Restructured code in L2 E3

--- Lab 2/Lab02_E03.s
+++ Lab 2/Lab02_E03.s
@@ -10,11 +10,11 @@ main:
     la a1,F            # a1 points to the array F. F will have the desired values at the end of the execution.
     
     #Calculating the last elements address
-    li a2,N            #length of Array
-    li a0,4            #length of a word. a0 will be 0 at the end of the program.
+    li a2,N            # length of Array
+    li a0,4            # length of a word. a0 will be 0 at the end of the program.
     mul a2,a2,a0       # word count * No of elements
-    addi a2,a2,-4     #offset correction. Without this, the address will become the address of the immediate next address at the end of the array.
-    add a2,a2,a1      #This holds F's last elements address. This will be the loop bound.
+    addi a2,a2,-4      # offset correction. Without this, the address will become the address of the immediate next address at the end of the array.
+    add a2,a2,a1       # This holds F's last elements address. This will be the loop bound.
     
     #Since first two numbers are the base cases, we have to explicitly put them to the array.
     #t0 and t1 will keep the previous values needed for next steps.
@@ -23,19 +23,16 @@ main:
    
     sw t0,0(a1)        # Storing first element in memory.
     addi a1,a1,4       # Incrementing the address to the next element.
-    sw t1,0(a1)        #Storing second element in memory.
+    sw t1,0(a1)        # Storing second element in memory.
     
     fibLoop:
-        li a0,0            #Resetting a0
+        li a0,0        # Resetting a0
         bge a1,a2,end
-         add a0,t1,t0
-        addi a1,a1,4   #Incrementing the address to next element.
+        add a0,t1,t0
+        addi a1,a1,4   # Incrementing the address to next element.
         sw a0,0(a1)    
-          mv t0,t1       #Copying value of t1 to t0
-          mv t1,a0
-       
-   
-        
+        mv t0,t1       # Copying value of t1 to t0
+        mv t1,a0
        
         j fibLoop
     
